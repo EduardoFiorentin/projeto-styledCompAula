55bd5dd84d3b230697349a7371fa3e06b7b4a3d0
Eduardo Fiorentin <95770285+EduardoFiorentin@users.noreply.github.com>
2022-03-14 10:32:39 -0300

    Initial commit

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,14 @@
+import {fundoClaro, conteudoClaro, textoFundoClaro, fundoEscuro, conteudoEscuro, textoFundoEscuro} from './variaveis'
+
+export const TemaClaro = {
+    body: fundoClaro, 
+    inside: conteudoClaro, 
+    text: textoFundoClaro,
+    filter: ''
+}
+export const TemaEscuro = {
+    body: fundoEscuro, 
+    inside: conteudoEscuro, 
+    text: textoFundoEscuro,
+    filter: "invert(100%)"
+}
